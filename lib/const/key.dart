7d362c574016b9be8f9fa3556c453a11bfab0795
philip inegbedion <phillipinegbedion@gmail.com>
2022-03-31 10:42:33 +0100

    moved key from git-ignore

---
+++ lib/const/key.dart
@@ -0,0 +1,10 @@
+const String movieURL =
+    'https://api.themoviedb.org/3/movie/popular?api_key=6747bc1219222d8fa82141689b1cfbf3&language=en-US&page=1';
+
+const String baseImageURL = 'https://image.tmdb.org/t/p/original/';
+
+const String upcomingMovieURL =
+    'https://api.themoviedb.org/3/movie/upcoming?api_key=6747bc1219222d8fa82141689b1cfbf3&language=en-US&page=1';
+
+const String trendingMoviesURL =
+    'https://api.themoviedb.org/3/trending/all/day?api_key=6747bc1219222d8fa82141689b1cfbf3';
